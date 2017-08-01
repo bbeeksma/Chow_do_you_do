e91b2ff3d2c073d99d6dd7093e7ed3d97070a2a7
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-01 16:15:49 -0500

    put the stuff I accidentally over-wrote back

--- public/scripts/controller/routes.js
+++ public/scripts/controller/routes.js
@@ -1,4 +1,3 @@
-
 var caloriesMin = 'gte%20200'; //format as gte%20<number>
 var caloriesMax = 'lte%20900'; //format as lte%20<number>
 var health = 'peanut-free';
@@ -20,15 +19,21 @@ tree-nut-free,
 alcohol-free
 */
 
-var app = app || {};
-
-page('/', app.homeController.show);
-page('/about', app.aboutController.show);
-page('/nutrition', app.nutritionController.show);
-page('/recipes', app.recipeController.show);
-
-page();
-
+/*
+  name: object.label
+  image: object.image
+  serving count: object.yield
+  calorie count: object.calories
+  ingredients: object.ingredietLines(array)
+  recipe link: object.url
+  carb count: object.totalNutrients.CHOCDF.quantity
+    carb unit:object.object.totalNutrients.CHOCDF.unit
+  protein: object.totalNutrients.PROCNT.quantity
+    protein unit: object.totalNutrients.PROCNT.unit
+  fat: object.totalNutrients.FAT.quantity
+    fat: object.totalNutrients.FAT.unit
+*/
+var recipeResults;
 getRecipe = () => {
   $.ajax({
     url: '/edamam/'
@@ -41,9 +46,11 @@ getRecipe = () => {
       ,health:'peanut-free'
       ,diet:'low-carb'
     }
-  })
-  .then(data => {
-    console.log(data);
-  }
-  );
+  }).then(data => {
+    recipeResults = data.hits.map(function(item){
+      return item.recipe;
+    });
+
+    console.log(recipeResults);
+  });
 };
