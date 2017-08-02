570ea8a67bfac94e59202fa58a2e1c2583735b18
Benjamin Beeksma <direwolf_85@yahoo.com>
2017-08-02 10:22:19 -0500

    client side route for not found added

--- public/scripts/controller/routes.js
+++ public/scripts/controller/routes.js
@@ -59,4 +59,9 @@ page('/', app.homeController.show);
 page('/about', app.aboutController.show);
 page('/nutrition', app.nutritionController.show);
 page('/recipes', app.recipeController.show);
+
+page('*', function(){
+  $('body').text('Not Found');
+});
+
 page();
