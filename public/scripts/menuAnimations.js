9a3e1ab378dec0ebce3fdb95796ae62376388ebf
ByteMiBro <bytemibro@gmail.com>
2017-08-02 17:34:57 -0500

    added scalable sign in input and button on a transparent backround

--- public/scripts/menuAnimations.js
+++ public/scripts/menuAnimations.js
@@ -1,8 +1,18 @@
+/*var shoo = function (){
+  $('.toggleMenu').on('click', function(){
+    $('.mainNav').toggleClass('show');
+    $('.toggleMenu').toggleClass('fa-toggle-down').toggleClass('fa-toggle-up');
+  });
+};
+var pow = function (){
+  $();
+};*/
 $('.toggleMenu').on('click', function(){
   $('.mainNav').toggleClass('show');
   $('.toggleMenu').toggleClass('fa-toggle-down').toggleClass('fa-toggle-up');
 });
-
 $(document).ready(function(){
+  //later I will need to check local storage before rendering it.
+  $('.sign-in').show();
   $('#home').show();
 });
