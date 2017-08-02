250bd6d8980b879b03797edec8ebd0cd062b5254
Dustin <dyelland@gmail.com>
2017-08-02 11:18:51 -0500

    wired the button to get a random recipe

--- public/scripts/model/model.js
+++ public/scripts/model/model.js
@@ -10,8 +10,8 @@ var app = app || {};
     this.image = object.image;
     this.servingCount = object.yield;
     this.calorieCount = object.calories;
-    this.ingredients = object.ingredietLines;
     this.recipeLink = object.url;
+    this.ingredients = Recipe.buildIngredientsList(object.ingredientLines);
     if (object.totalNutrients.CHOCDF){
       this.carbCount = object.totalNutrients.CHOCDF.quantity + ' ' + object.totalNutrients.CHOCDF.unit;
     } else {
@@ -29,6 +29,17 @@ var app = app || {};
     }
   }
 
+  //returns ingredients as a list of <li> elements to append with the tmplate
+  Recipe.buildIngredientsList = function(ingredients){
+    var allIngredients = ingredients.map(function(item){
+      return '<li>' + item + '</li>';
+    }).reduce(function(acc,val){
+      return acc += val;
+    });
+    console.log('ingredients for template: ',allIngredients);
+    return allIngredients;
+  };
+
   Recipe.loadRecipes = function(){
     Recipe.all = recipeResults.map(function(recipe){
       return new Recipe(recipe);
@@ -41,15 +52,19 @@ var app = app || {};
     return template(recipe);
   };
 
-  Recipe.getDisplayRecipes = function(){
-    //var randomRecipe = Math.floor(Math.random() * Recipe.all.length);
+  Recipe.getRandomRecipe = function(){
+    return Math.floor(Math.random() * Recipe.all.length);
   };
 
   Recipe.initRecipes = function(){
     Recipe.loadRecipes();
-    console.log('Recipes Loaded');
-    $('#recipe1').empty().append(Recipe.toHtml(Recipe.all[0]));
+    var thisRecipe = Recipe.getRandomRecipe();
+    $('#recipes').empty().append(Recipe.toHtml(Recipe.all[thisRecipe]));
   };
+  //Get Recipes Handler function: Feel free to move this whare necessary
+  $('#button').on('click', function(){
+    Recipe.initRecipes();
+  });
 
   module.Recipe = Recipe;
 })(app);
