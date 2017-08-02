b1ee009d0733fbb3d139b087f464c4a0a4fd49d5
nerdbetter <jasontlogan@gmail.com>
2017-08-02 11:47:53 -0500

    menu now closes when selecting a page

--- public/scripts/menuAnimations.js
+++ public/scripts/menuAnimations.js
@@ -2,7 +2,11 @@ $('.toggleMenu').on('click', function(){
   $('.mainNav').toggleClass('show');
   $('.toggleMenu').toggleClass('fa-toggle-down').toggleClass('fa-toggle-up');
 });
+$('.mainNav a').on('click', function(){
+  $('.mainNav').toggleClass('show');
+  $('.toggleMenu').toggleClass('fa-toggle-down').toggleClass('fa-toggle-up');
 
+});
 $(document).ready(function(){
   $('#home').show();
 });
