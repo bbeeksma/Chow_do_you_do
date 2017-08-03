17cef2dfd029706d5e08f6c7f3e4da4c8af67ac5
Dustin <dyelland@gmail.com>
2017-08-03 18:45:27 -0500

    testing 10px and removed swipe up/down completely

--- public/scripts/view/swipe.js
+++ public/scripts/view/swipe.js
@@ -20,34 +20,37 @@ function handleTouchMove(event) {
   var yDiff = yDown - yUp;
 
   if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-    if ( xDiff > 15 ) {
+    if ( xDiff > 10 ) {
           /* left swipe */
       console.log('swiped left');
       $(this).hide('slide', { direction: 'left' }, 200);
       //TODO:Get next recipe
       app.Recipe.getNextRecipe(event,$(event.target).closest('div'));
-    } else if ( xDiff < -15 ) {
+    } else if ( xDiff < -10 ) {
       /* right swipe */
       console.log('swiped right');
       $(this).hide('slide', { direction: 'right' }, 200);
       //TODO:get previous recipe
       app.Recipe.getPreviousRecipe(event,$(event.target).closest('div'));
     }
-  } else {
+  }
+/*
+  else {
     if ( yDiff > 15 ) {
-      /* up swipe */
+      /* up swipe
       console.log('swiped up');
       $(this).hide('slide', { direction: 'up' }, 200);
       //TODO:Delete recipe
       //app.Recipe.discardRecipe(event,$(event.target).closest('div'));
     } else if ( yDiff < -15 ) {
-      /* down swipe */
+      /* down swipe
       console.log('swiped down');
       $(this).hide('slide', { direction: 'down' }, 200);
       //TODO:Save Recipe
       //app.Recipe.saveRecipe();
     }
   }
+  */
   /* reset values */
   xDown = null;
   yDown = null;
