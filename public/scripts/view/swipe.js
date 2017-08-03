cc7820790d8a7048c5f9457b136c9a36f22e0de2
Dustin <dyelland@gmail.com>
2017-08-03 18:35:01 -0500

    changed swioe on 15 px

--- public/scripts/view/swipe.js
+++ public/scripts/view/swipe.js
@@ -20,13 +20,13 @@ function handleTouchMove(event) {
   var yDiff = yDown - yUp;
 
   if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-    if ( xDiff > 30 ) {
+    if ( xDiff > 15 ) {
           /* left swipe */
       console.log('swiped left');
       $(this).hide('slide', { direction: 'left' }, 200);
       //TODO:Get next recipe
       app.Recipe.getNextRecipe(event,$(event.target).closest('div'));
-    } else if ( xDiff < -30 ) {
+    } else if ( xDiff < -15 ) {
       /* right swipe */
       console.log('swiped right');
       $(this).hide('slide', { direction: 'right' }, 200);
@@ -34,13 +34,13 @@ function handleTouchMove(event) {
       app.Recipe.getPreviousRecipe(event,$(event.target).closest('div'));
     }
   } else {
-    if ( yDiff > 30 ) {
+    if ( yDiff > 15 ) {
       /* up swipe */
       console.log('swiped up');
       $(this).hide('slide', { direction: 'up' }, 200);
       //TODO:Delete recipe
       app.Recipe.discardRecipe(event,$(event.target).closest('div'));
-    } else if ( yDiff < -30 ) {
+    } else if ( yDiff < -15 ) {
       /* down swipe */
       console.log('swiped down');
       $(this).hide('slide', { direction: 'down' }, 200);
